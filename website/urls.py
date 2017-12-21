8cfd8c6f7f07052c26dc7e47d38026dd5e0ff46a
rensoostenbach <rensoostenbach@hotmail.com>
2017-12-21 10:10:40 +0100

    Added favicon v2

--- website/urls.py
+++ website/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^favicon.ico$', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'), permanent=False), name="favicon"),
+    url(r'^favicon.ico$', RedirectView.as_view(url='/static/favicon/favicon.ico')),
     url(r'^contact/', views.contact, name='contact'),
     url(r'^hunterbuild/$', views.hunter_build, name='hunter_build'),
     url(r'^hunterbuild/(?P<pk>\d+)/$', views.hunterbuild_detail, name='hunterbuild_detail'),
